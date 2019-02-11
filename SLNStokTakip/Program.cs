d97d672efe48e948ebc363f3db9f19f65350849f
gamze türkmen <gamze7289@hotmail.com>
2019-02-12 02:36:27 +0300

    s

--- SLNStokTakip/Program.cs
+++ SLNStokTakip/Program.cs
@@ -16,7 +16,7 @@ namespace SLNStokTakip
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new AnaSayfa());
+            Application.Run(new AnaSayfa()); 
         } 
     }
 }
